bbef1e8b5d5457333242baf0808ee1fbae3aaf54
mike <youbob1212@gmail.com>
2021-07-20 22:27:23 -0700

    added in some js to the JSX'

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+function getMood(){
+    const moods = ['Angry','Hungry', 'Silly', 'Quiet', 'Paranoid', 'Happy']
+    return moods[Math.floor(Math.random() * moods.length)]; 
+
+}
+
+
 class JSXDemo extends React.Component {
     render() {
         return(
             <div>
                 
                     <img src="http://placekitten.com/g/400/500" width="900" height="800"/>
+                    <h1>My Current Kitty mood is: {getMood()}</h1>
                 
             </div>
         );
