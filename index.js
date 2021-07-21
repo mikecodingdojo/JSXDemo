a581fe327cf2d9849819adce7cc1e596301df464
mike <youbob1212@gmail.com>
2021-07-20 22:14:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+class JSXDemo extends React.Component {
+    render() {
+        return(
+            <div>
+                
+                    <img src="http://placekitten.com/g/400/500" width="900" height="800"/>
+                
+            </div>
+        );
+
+    }
+}
+
+ReactDOM.render(<JSXDemo/>, document.getElementById('root')); 
